d4a6cfbbd4ea19fe82f165eda5c98eef54092d3f
Josue Arreola <josue@heroguest.com>
2024-02-21 02:23:05 -0600

    update home url

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
     // PROD
     //homeUrl: 'https://josue.ngrok.io/api/admin',
-    homeUrl: 'http://localhost:3333',
+    homeUrl: 'https://josue.ngrok.io',
     //homeUrl: 'https://grupo-locsa-core.onrender.com/api/admin',
     defaultTimezone: 'America/Mexico_City',
     imagesUrl: 'https://torniweb.s3.amazonaws.com/',
